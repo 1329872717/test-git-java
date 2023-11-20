4f06f22368d354947876f8936b02f796516a7652
1329872717 <1329872717@qq.com>
2023-11-20 22:03:55 +0800

    嘻嘻

---
+++ codetest_/src/Unuseful/Test.java
@@ -0,0 +1,17 @@
+package Unuseful;
+
+import demo2.Solution;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        Solution solution = new Solution();
+    //    solution.numSquares(5);
+        System.out.println(150*54);
+
+
+
+    }
+}
